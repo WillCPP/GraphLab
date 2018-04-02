b084d49baa00b33c7423e5538e23d03ed33a9573
WillCPP <WillCPP@users.noreply.github.com>
2018-04-02 16:01:04 -0400

    Change Node to Edge in Graph.h

--- Graph.h
+++ Graph.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "Edge.h"
+#include "Vertice.h"
 
 class Graph
 {
-	Node **data;
+	Vertice **data;
 	int MAXSIZE;
 	Graph() {}
 	Graph(int max = 100) {
 		MAXSIZE = max;
-		data = new Node[MAXSIZE]();
+		data = new Vertice*[MAXSIZE]();
 	}
 	~Graph() {}
 
