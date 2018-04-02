9a807433859756bebfc15791081c9e55c692ace9
WillCPP <WillCPP@users.noreply.github.com>
2018-04-02 16:19:59 -0400

    Merged Graph.h

--- Graph.h
+++ Graph.h
@@ -4,36 +4,23 @@
 
 class Graph
 {
-	Node **data;
 	Vertice **data;
 	int MAXSIZE;
 	Graph() {}
 	Graph(int max = 100) {
 		MAXSIZE = max;
-		data = new Node[MAXSIZE]();
-
 		data = new Vertice*[MAXSIZE]();
 	}
 	~Graph() {}
 
 	
 public:
-<<<<<<<<< saved version
-		data = new Vertice*[MAXSIZE]();
-	}
-	~Graph() {}
-
-	
-public:
-	void addNode() {}
-=========
 	void addNode(int *i) {}
 	void removeEdge(int *i, int *j) {}
 	void addEdge(int *i, int *j) {}
     bool hasEdge(int *i, int *j) {}
->>>>>>>>> local version
-    outEdge() {}
-    inEdge() {}
+    int outEdge() {}
+    int inEdge() {}
     void displayGraph() {}
 
 };
