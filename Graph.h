10e5153ffad7fb11c9985dd1065d8d23fb4f454f
Sean Hearne <hearnesc@mail.uc.edu>
2018-04-02 16:36:33 -0400

    Outedge Remove add and has

--- Graph.h
+++ Graph.h
@@ -19,13 +19,24 @@ public:
 		data[i] = new Vertice(i);
 	}
 	void removeEdge(int i, int j) {
-		data[i]->edges.getItem(j)
+		data[i]->edges.getItem(j);
 	}
 	void addEdge(int i, int j) {
 		data[i]->edges.addItem(j);
 	}
-    bool hasEdge(int i, int j) {}
-    int outEdge() {}
+    bool hasEdge(int i, int j) {
+		data[i]->edges.findItem(j);
+	}
+    int[] outEdge() {
+		int size = data[i]->edges.size();
+		int retArr[size];
+		data[i]->edges.seeAt(0);
+		for (int i = 0; i < size; i++) {
+			int j = data[i]->edges.seeNext().data;
+			retArr[i] = j;
+		}
+		return retArr;
+	}
     int inEdge() {}
     void displayGraph() {}
 
