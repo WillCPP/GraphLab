eb829d1e5831e53095973e638df3dd241f11acc1
WillCPP <WillCPP@users.noreply.github.com>
2018-04-02 14:36:06 -0400

    Add project files.

---
+++ Graph.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Graph
+{
+	Graph() {}
+	~Graph() {}
+
+public:
+	addNode();
+
+};
---
+++ Node.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Node {
+	Node() {}
+	Node(char c) {}
+public:
+	char Name = '0';
+};
